6e517329fee00cb6c6587975cd2b7cb7b42685d2
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-08-29 12:15:03 -0500

    Add datatable view response

--- maestro/views.py
+++ maestro/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, render_to_response, redirect
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from django.http import JsonResponse
 
 # Create your views here.
-@login_required
+
 def index(request):
   user_name = request.user.username
   return render_to_response('maestro/index.html',RequestContext(request,locals()))
+
+def datatable(request):
+  # return JsonResponse({'foo':'bar'})
+  return render_to_response('blblbl')
