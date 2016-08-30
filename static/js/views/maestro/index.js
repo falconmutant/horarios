050467482f94688326973af735d03a21cdb0a6c0
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-08-30 00:07:29 -0500

    Add javascript file to index maestro view

--- static/js/views/maestro/index.js
+++ static/js/views/maestro/index.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-  $('#example').DataTable( {
-      "ajax": 'blblblbl'
+  $('#dt_maestro').DataTable( {
+    "ajax": 'datatable/',
+    // columns:{
+    //   data: 'Nombre',
+    //   data: 'Apellido_Paterno',
+    //   data: 'Apellido_Materno',
+    //   data: 'Contrato',
+    //   data: 'Proyecto_Financiado',
+    //   data: 'Grado_De_Estudio',
+    //   data: 'Clase_Maestria',
+    //   data: 'Num_Materias'
+    // }
   } );
-  console.log('from here ');
 } );
