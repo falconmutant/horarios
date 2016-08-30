98058df6d0353ee2800815ce2bffb3540292a451
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-08-30 00:03:45 -0500

    Add model Maestro, its migration, settings configuration

--- maestro/admin.py
+++ maestro/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Maestro
+
+admin.site.register(Maestro)
--- maestro/models.py
+++ maestro/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Maestro(models.Model):
+  Nombre = models.CharField(max_length=50)
+  Apellido_Paterno = models.CharField(max_length=50)
+  Apellido_Materno = models.CharField(max_length=50)
+  Contrato = models.CharField(max_length=50)
+  Proyecto_Financiado = models.CharField(max_length=2)
+  Grado_De_Estudio = models.CharField(max_length=50)
+  Clase_Maestria = models.CharField(max_length=2)
+  Num_Materias = models.SmallIntegerField()
