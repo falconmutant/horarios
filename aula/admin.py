77c3bae376c0e05352ef5cd2cd47361a30d4e897
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-08-30 21:24:29 -0500

    Modify admin config on aula

--- aula/admin.py
+++ aula/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Aula)
-admin.site.register(Aula_disponibilidad)
-admin.site.register(Aula_software)
+admin.site.register(Aula_Disponibilidad)
+admin.site.register(Aula_Software)
+admin.site.register(Aula_Materia)
