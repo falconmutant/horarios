a434de48d3308d0bfdd07103bd6de8d10ab79efa
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-08-30 21:31:06 -0500

    Aggregate Grupo models app

--- grupo/models.py
+++ grupo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from modulos.models import Turno
+from materia.models import Materia
 
-# Create your models here.
+class Grupo(models.Model):
+  Nombre = models.TextField(max_length=50, null=False)
+  Tipo_Grupo = models.TextField(max_length=15,null=False)
+  Id_Turno = models.ForeignKey(Turno)
+
+class Grupo_Materia(models.Model):
+  Id_Materia = models.ForeignKey(Materia)
+  Id_Grupo = models.ForeignKey(Grupo)
