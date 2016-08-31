672a384d1c4b1484a57f455cca5fd142ea6df936
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-08-30 21:28:58 -0500

    Aggregate Models for aula app

--- aula/models.py
+++ aula/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from modulos.models import *
+from materia.models import Materia
 # Create your models here.
 class Aula(models.Model):
-	Nombre = models.TextField(null=False)
-	Capacidad = models.TextField(null=False)
-	Id_Tipo_de_aula = models.ForeignKey(Tipo_de_aula)
-	Id_Planta_Edificio = models.ForeignKey(Planta_edificio)
-	def __str__(self):
-		return "%s" % (self.Nombre)
+    Nombre = models.TextField(null=False)
+    Capacidad = models.TextField(null=False)
+    Id_Tipo_de_aula = models.ForeignKey(Tipo_de_aula)
+    Id_Planta_Edificio = models.ForeignKey(Planta_edificio)
 
-class Aula_disponibilidad(models.Model):
-	Id_Disponibilidad = models.ForeignKey(Disponibilidad)
-	Id_Aula = models.ForeignKey(Aula)
+class Aula_Disponibilidad(models.Model):
+    Id_Disponibilidad = models.ForeignKey(Disponibilidad)
+    Id_Aula = models.ForeignKey(Aula)
 
-class Aula_software(models.Model):
-	Id_Software = models.ForeignKey(Software)
-	Id_Aula = models.ForeignKey(Aula)
+class Aula_Software(models.Model):
+    Id_Software = models.ForeignKey(Software)
+    Id_Aula = models.ForeignKey(Aula)
+
+class Aula_Materia(models.Model):
+    Horas_Obligatorias =  models.SmallIntegerField()
+    Peso = models.SmallIntegerField()
+    Id_Aula = models.ForeignKey(Aula)
+    Id_Materia = models.ForeignKey(Materia)
