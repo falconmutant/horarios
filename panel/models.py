bac328d621fd9334dd5671350897aaa66f3ca8a1
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-08-30 21:36:31 -0500

    Add panel app models

--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from modulos.models import Disponibilidad
+from aula.models import Aula
+from maestro.models import Maestro
+from grupo.models import Grupo_Materia
 
-# Create your models here.
+class Solucion(models.Model):
+  Costo = models.IntegerField()
+  Id_Disponibilidad = models.ForeignKey(Disponibilidad)
+  Id_Aula = models.ForeignKey(Aula)
+  Id_Maestro = models.ForeignKey(Maestro)
+  Id_Grupo_Materia = models.ForeignKey(Grupo_Materia)
