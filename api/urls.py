887c8ef3e41cbfb10025b7caf5495938a17799e3
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-09-01 07:08:59 -0500

    Add url to MateriaDetail on api

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
   url(r'^maestro/$', views.MaestroList.as_view()),
   url(r'^maestro/(?P<pk>[0-9]+)/$', views.MaestroDetail.as_view()),
   url(r'^materia/$', views.MateriaList.as_view()),
+  url(r'^materia/(?P<pk>[0-9]+)/$', views.MateriaDetail.as_view()),
   url(r'^software/$', views.SoftwareList.as_view()),
   url(r'^planta/$', views.PlantaList.as_view()),
   url(r'^edificio/$', views.EdificioList.as_view()),
