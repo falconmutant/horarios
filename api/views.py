b49eeeff3e3eeac1591dfd499d92d5052bdd8b5e
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-09-01 06:24:19 -0500

    MaestroDetail class on view api

--- api/views.py
+++ api/views.py
@@ -83,6 +83,31 @@ class MaestroList(APIView):
     serializer = MaestroSerializer(maestro, many = True)
     return Response(serializer.data)
 
+class MaestroDetail(APIView):
+  def get_object(self, pk):
+    try:
+      return Maestro.objects.get(pk=pk)
+    except Maestro.DoesNotExist:
+      raise Http404
+
+  def get(self, request, pk, format=None):
+    maestro = self.get_object(pk)
+    serializer = MaestroSerializer(maestro)
+    return Response(serializer.data)
+
+  def put(self, request, pk, format=None):
+    maestro = self.get_object(pk)
+    serializer = MaestroSerializer(maestro, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    maestro = self.get_object(pk)
+    maestro.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
 class MateriaList(APIView):
   def get(self, request, format=None):
     materia = Materia.objects.all()
