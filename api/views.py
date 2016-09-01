27ccb44260657283f4ed3aa60c65212d4a8f7496
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-09-01 07:21:39 -0500

    SoftwareDetail view on api

--- api/views.py
+++ api/views.py
@@ -145,6 +145,31 @@ class SoftwareList(APIView):
     serializer = SoftwareSerializer(model,many=True)
     return Response(serializer.data)
 
+class SoftwareDetail(APIView):
+  def get_object(self, pk):
+    try:
+      return Software.objects.get(pk=pk)
+    except Software.DoesNotExist:
+      raise Http404
+
+  def get(self, request, pk, format=None):
+    software = self.get_object(pk)
+    serializer = SoftwareSerializer(software)
+    return Response(serializer.data)
+
+  def put(self, request, pk, format=None):
+    software = self.get_object(pk)
+    serializer = SoftwareSerializer(software, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    software = self.get_object(pk)
+    software.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
 class PlantaList(APIView):
   def get(self, request, format=None):
     model = Planta.objects.all()
