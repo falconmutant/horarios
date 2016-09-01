9cdee4136e7af21ba667b353fbfd6ed4d2045b9c
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-09-01 06:24:44 -0500

    Modify fields on Maestro Serializer

--- maestro/serializers.py
+++ maestro/serializers.py
@@ -4,6 +4,6 @@ from .models import Maestro
 class MaestroSerializer(serializers.ModelSerializer):
   class Meta:
     model = Maestro
-    fields = ('Nombre', 'Apellido_Paterno', 'Apellido_Materno', 'Contrato',
+    fields = ('id','Nombre', 'Apellido_Paterno', 'Apellido_Materno', 'Contrato',
       'Proyecto_Financiado', 'Grado_De_Estudio', 'Clase_Maestria',
       'Num_Materias')
