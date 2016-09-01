fcddc34baac7eaa7e7a0626cec87c4306c674ed9
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-09-01 07:09:34 -0500

    Add MateriaDetail view api

--- api/views.py
+++ api/views.py
@@ -114,6 +114,31 @@ class MateriaList(APIView):
     serializer = MateriaSerializer(materia,many=True)
     return Response(serializer.data)
 
+class MateriaDetail(APIView):
+  def get_object(self, pk):
+    try:
+      return Materia.objects.get(pk=pk)
+    except Materia.DoesNotExist:
+      raise Http404
+
+  def get(self, request, pk, format=None):
+    materia = self.get_object(pk)
+    serializer = MateriaSerializer(materia)
+    return Response(serializer.data)
+
+  def put(self, request, pk, format=None):
+    materia = self.get_object(pk)
+    serializer = MateriaSerializer(materia, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+      materia = self.get_object(pk)
+      materia.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
+
 class SoftwareList(APIView):
   def get(self, request, format=None):
     model = Software.objects.all()
