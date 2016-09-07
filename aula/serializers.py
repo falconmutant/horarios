4bd90b1a21749f561ec15e8120570c69cd07b5b0
Luis Aguilera <aguilera.glz.luis@gmail.com>
2016-09-07 02:24:26 -0500

    delete aula_disponibilidad on api

--- aula/serializers.py
+++ aula/serializers.py
@@ -4,8 +4,3 @@ from aula.models import *
 class AulaSerializer(serializers.ModelSerializer):
   class Meta:
     model = Aula
-    fields = ('id','Nombre','Capacidad', 'Id_Tipo_de_aula', 'Id_Planta_Edificio')
-
-class Aula_Disponibilidad_Serializer(serializers.ModelSerializer):
-  class Meta:
-      model = Aula_Disponibilidad
